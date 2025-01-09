f01128b2ad217dca24a60b623a84cee4c4eb87c4
camillazicari <camyzicari01@gmail.com>
2025-01-09 15:44:17 +0100

    es 1

--- libreria/src/App.jsx
+++ libreria/src/App.jsx
@@ -4,6 +4,7 @@ import MyNav from './components/MyNav';
 import MyFooter from './components/MyFooter';
 import Welcome from './components/Welcome';
 import AllTheBooks from './components/AllTheBooks';
+import SingleBook from './components/SingleBook';
 
 function App() {
   //const [count, setCount] = useState(0)
@@ -11,8 +12,13 @@ function App() {
   return (
     <>
       <MyNav />
+
+      <SingleBook img='https://images-na.ssl-images-amazon.com/images/I/91vVbxOztSL.jpg' title='Over the Edge  (Troubleshooters series, Book 3)' price={26.04} category='romance' />
       <Welcome />
       <AllTheBooks />
+
+
+
       <MyFooter />
     </>
   )
--- libreria/src/components/SingleBook.jsx
+++ libreria/src/components/SingleBook.jsx
@@ -1,37 +1,31 @@
-import { Container, Row, Col } from "react-bootstrap";
-import books from "../data/romance.json";
-import Card from "react-bootstrap/Card";
+import { Container, Row, Col, Card } from "react-bootstrap";
 
-const AllTheBooks = function () {
-  return (
-    <Container fluid>
+const SingleBook = (props) => {
+    return (
+        <Container>
       <Row className='flex-wrap'>
-        {books.map((book) => {
-          return (
-            <Col xs={4} key={book.asin} className='mb-4'>
+            <Col xs={3}className='mb-4'>
               <Card style={{height: "700px", width:"400px"}}>
                 <Card.Img
-                  src={book.img}
+                  src={props.img}
                   alt='Card image'
                   style={{objectFit: "cover", objectPosition:"top"}}
                   className="h-75"
                 />
                 <Card.Body className="d-flex flex-column justify-content-evenly h-25 py-0">
-                    <h6 className="text-center">{book.title}</h6>
-                    <p className="btn btn-warning btn-sm w-50 align-self-center"> {book.category} </p>
+                    <h6 className="text-center">{props.title}</h6>
+                    <p className="btn btn-warning btn-sm w-50 align-self-center"> {props.category} </p>
                     <div className="d-flex justify-content-evenly">
-                    <p className="m-0 align-self-center">€{book.price}</p>
+                    <p className="m-0 align-self-center">€{props.price}</p>
                     <button type="button" className="btn btn-sm bg-success text-white m-0 align-self-center"><i className="bi bi-cart-check"></i> Add to cart</button>   
                     </div>
                     
                 </Card.Body>
               </Card>
             </Col>
-          );
-        })}
       </Row>
     </Container>
-  );
-};
+    )
+}
 
-export default AllTheBooks;
+export default SingleBook;
