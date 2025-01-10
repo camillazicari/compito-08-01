1cd8df4293df2c1b51258bc6df60710096928483
camillazicari <camyzicari01@gmail.com>
2025-01-10 02:32:40 +0100

    search

--- libreria/src/components/BookList.jsx
+++ libreria/src/components/BookList.jsx
@@ -1,19 +1,40 @@
 import { Component } from "react";
 import SingleBook from "./SingleBook";
+import { Form } from "react-bootstrap";
 
 class BookList extends Component {
+  state = {
+    inputValue: "",
+  };
+
   render() {
-    return this.props.scifi.map((book) => {
-      return (
-        <SingleBook
-          key={book.asin}
-          img={book.img}
-          title={book.title}
-          price={book.price}
-          category={book.price}
+    return (
+      <>
+        <Form.Control
+          onChange={(e) => {
+            this.setState({ inputValue: e.target.value });
+          }}
+          value={this.state.inputValue}
+          type='text'
+          placeholder='cerca libri...'
+          className='my-3'
         />
-      );
-    });
+
+        {this.state.inputValue === ""
+          ? this.props.scifi.map((book) => {
+              return <SingleBook bookProp={book} key={book.asin} />;
+            })
+          : this.props.scifi
+              .filter((book) =>
+                book.title
+                  .toLowerCase()
+                  .includes(this.state.inputValue.toLowerCase())
+              )
+              .map((book) => {
+                return <SingleBook key={book.asin} bookProp={book} />;
+              })}
+      </>
+    );
   }
 }
 
--- libreria/src/components/SingleBook.jsx
+++ libreria/src/components/SingleBook.jsx
@@ -2,47 +2,56 @@ import { Container, Row, Col, Card } from "react-bootstrap";
 import { Component } from "react";
 
 class SingleBook extends Component {
+  state = {
+    selected: false,
+  };
 
-    state = {
-        selected: false
-    }
-
-    render() {
+  render() {
     return (
-        <Container>
-      <Row className='flex-wrap'>
-            <Col xs={3}className='mb-4'>
-              <Card style={{height: "700px", width:"400px"}}                 
-              className={this.state.selected ? 'border-danger border-5' : 'border-0'}
-              >
-                <Card.Img
-                onClick={ () => {
-                    if (this.state.selected) {
-                        this.setState({selected: false})
-                    } else {
-                        this.setState({selected: true})
-                    }
+      <Container>
+        <Row className='flex-wrap'>
+          <Col xs={3} className='mb-4'>
+            <Card
+              style={{ height: "700px", width: "400px" }}
+              className={
+                this.state.selected ? "border-danger border-5" : "border-1 border-secondary"
+              }
+            >
+              <Card.Img
+                onClick={() => {
+                  if (this.state.selected) {
+                    this.setState({ selected: false });
+                  } else {
+                    this.setState({ selected: true });
+                  }
                 }}
-                className="h-75"
-                  src={this.props.img}
-                  alt='Card image'
-                  style={{objectFit: "cover", objectPosition:"top"}}
-                />
-                <Card.Body className="d-flex flex-column justify-content-evenly h-25 py-0">
-                    <h6 className="text-center">{this.props.title}</h6>
-                    <p className="btn btn-warning btn-sm w-50 align-self-center"> {this.props.category} </p>
-                    <div className="d-flex justify-content-evenly">
-                    <p className="m-0 align-self-center">€{this.props.price}</p>
-                    <button type="button" className="btn btn-sm bg-success text-white m-0 align-self-center"><i className="bi bi-cart-check"></i> Add to cart</button>   
-                    </div>
-                    
-                </Card.Body>
-              </Card>
-            </Col>
-      </Row>
-    </Container>
-    )
-}
+                className='h-75'
+                src={this.props.bookProp.img}
+                alt='Card image'
+                style={{ objectFit: "cover", objectPosition: "top" }}
+              />
+              <Card.Body className='d-flex flex-column justify-content-evenly h-25 py-0'>
+                <h6 className='text-center'>{this.props.bookProp.title}</h6>
+                <p className='btn btn-warning btn-sm w-50 align-self-center'>
+                  {" "}
+                  {this.props.bookProp.category}{" "}
+                </p>
+                <div className='d-flex justify-content-evenly'>
+                  <p className='m-0 align-self-center'>€{this.props.bookProp.price}</p>
+                  <button
+                    type='button'
+                    className='btn btn-sm bg-success text-white m-0 align-self-center'
+                  >
+                    <i className='bi bi-cart-check'></i> Add to cart
+                  </button>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default SingleBook;
